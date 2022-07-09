534567284aa09541b75f42ee0484d1df52138d14
Paipai13 <43116855+Paipai13@users.noreply.github.com>
2022-07-09 08:26:46 +0000

    Initial commit

---
+++ mad-libs.py
@@ -0,0 +1,17 @@
+fruit = input("Fruit: ")
+noun1 = input("Noun: ")
+name = input("Name: ")
+adj1 = input("Adjective: ")
+color=input("Color: ")
+adj2 = input("Adjective again: ")
+adj3 = input("Adjective yet again: ")
+noun2 = input("Noun again: ")
+verb1= input("Verb: ")
+verb2= input("Verb again : ")
+noun3 = input("Noun yet again: ")
+verb3= input("Verb yet again: ")
+animal = input("Animal: ")
+
+madlib = f" \n Ohhhhh... who lives in a {fruit} under the {noun1}? \n {name} Squarepants! \n {adj1} and {color} and {adj2} is he! \n {name} Squarepants! \n If {adj3} {noun2} be somthing you {verb1} \n {name} Squarepants! \n Then {verb2} on the {noun3} and {verb3} like a {animal}. \n {name} Squarepants!\n Ready? \n {name} Squarepants! \n {name} Squarepants! \n {name} Squarepants! \n {name}... Squarepants! ahhhahhhahahhaha! \n"
+
+print(madlib)
